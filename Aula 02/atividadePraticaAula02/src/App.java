80f35fa785fae66ab57c9f096512c2757270188e
João Carlos <joaocarlosreis2007@gmail.com>
2026-08-14 23:06:09 -0300

    feat: adição do atributo nome e do metodo "checkBalance", verificando o saldo

--- Aula 02/atividadePraticaAula02/src/App.java
+++ Aula 02/atividadePraticaAula02/src/App.java
@@ -1,14 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        ContaBancaria conta01 = new ContaBancaria(12345, 0.0);
+        ContaBancaria conta01 = new ContaBancaria(12345, 0.0, "Vinicius Souza");
 
         System.out.println(conta01);
-        
+        System.out.println("");
         conta01.depositar(500);
-        conta01.sacar(256);
-
         System.out.println(conta01);
+        System.out.println("");
+        conta01.sacar(256);
+        System.out.println(conta01);
+        System.out.println("");
+        System.out.println(conta01.checkBalance(12345));
+        System.out.println(conta01);
+        System.out.println("");
+
 
     }
 }
