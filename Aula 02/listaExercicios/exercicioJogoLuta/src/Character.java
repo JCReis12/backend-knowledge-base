9ed07c0de6480857612d4f1bc9461bc1f8f2ad7f
João Carlos <joaocarlosreis2007@gmail.com>
2026-08-21 14:18:45 -0300

    fix: atualização da lógica de ataque

--- Aula 02/listaExercicios/exercicioJogoLuta/src/Character.java
+++ Aula 02/listaExercicios/exercicioJogoLuta/src/Character.java
@@ -1,3 +1,17 @@
 public class Character {
-    
+    String name;
+    int life;
+    int attack;
+    int defense;
+
+    public void attack(Character opponent){
+        int damage = this.attack - opponent.defense;
+
+        if(damage > 0){
+            opponent.life -= damage;
+            System.out.println(opponent.name + " recebeu dano de " + damage);
+        }
+        System.out.println("Ataque de " + this.name + " defendido!");
+
+    }
 }
