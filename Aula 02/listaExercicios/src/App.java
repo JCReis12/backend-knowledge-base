26cd2d091d99bedb2fa00d23fd5954110ef90048
João Carlos <joaocarlosreis2007@gmail.com>
2026-08-20 21:52:14 -0300

    feat: adição do objeto de arquivos

--- Aula 02/listaExercicios/src/App.java
+++ Aula 02/listaExercicios/src/App.java
@@ -1,5 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Arquivo arq1 = new Arquivo();
+
+        arq1.name = "Anotações";
+        arq1.size = 32;
+        arq1.content = "Conteúdo inicial do meu arquivo primário";
+
+        System.out.println(arq1.name + " | " + arq1.size);
+        System.out.println("Conteúdo: " + arq1.content);
+
+        arq1.rename("Anotations");
+        
+        arq1.edit("New english content to my anotations");
+
+        arq1.clear();
     }
 }
