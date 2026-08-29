ddb0b38bde552a4cccf4c1151b4093167c378fe9
João Carlos <joaocarlosreis2007@gmail.com>
2026-08-28 21:15:56 -0300

    feat: adição de nova inicialização do objeto mais avançado

--- Aula 02/listaExercicios/exercicioEditorArquivo/src/ObjetoMaisAvancado.java
+++ Aula 02/listaExercicios/exercicioEditorArquivo/src/ObjetoMaisAvancado.java
@@ -2,34 +2,45 @@ import java.util.Scanner;
 public class ObjetoMaisAvancado {
     public static void main(String[] args) throws Exception {
         Scanner jc = new Scanner(System.in);
-        int opc = 0;
-        
-        Arquivo arq1 = new Arquivo();
+        String rsp = "s";
 
-        arq1.name = "Anotações";
-        arq1.size = 32;
-        arq1.content = "Conteúdo inicial do meu arquivo primário";
+        for(int i = 0; i < 80; i++){
+            System.out.println(" ");
+        }
 
+        System.out.println("============== BEM-VINDO AO BLOCO DE NOTAS ==============");
         do{
-            System.out.println("");
-            System.out.println("O que deseja realizar?");
-            System.out.println("1 - Abrir | 2 - Renomear | 3 - Editar conteúdo | 4 - Limpar | 5 - Sair");
-            opc = jc.nextInt();
+            System.out.print("Deseja criar um arquivo? (s/n) ");
+            rsp = jc.next();
+        }while(!rsp.equalsIgnoreCase("s"));
 
-            if(opc == 1){
-                System.out.println(arq1.name + " | " + arq1.size);
-                System.out.println("Conteúdo: " + arq1.content);
-            }
-            else if(opc == 2){
-                arq1.rename("Anotations");
-            }
-            else if(opc == 3){
-                arq1.edit("New english content to my anotations");
-            }
-            else if(opc == 4){
-                arq1.clear();
-            }
-        }while(opc != 5);
+
+        for(int i = 0; i < 80; i++){
+            System.out.println(" ");
+        }
+
+        System.out.print("Qual o nome do seu novo arquivo: ");
+        String arqNome = jc.next();
+
+        Arquivo arquivo = new Arquivo(arqNome);
+
+        for(int i = 0; i < 3; i++){
+            System.out.print(". ");
+            Thread.sleep(800);
+        }
+
+        System.out.println("");
+
+        System.out.println("Arquivo '" + arquivo.getName() + "' criado com sucesso!");
+
+        
+
+
+
+
+
+
+        
 
 
     }
