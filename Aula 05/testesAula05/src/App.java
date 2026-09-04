eca3be9a8a7281ba84f7fd72f04e3d90598161f6
João Carlos <joaocarlosreis2007@gmail.com>
2026-09-04 20:33:10 -0300

    feat: adição de try catch

--- Aula 05/testesAula05/src/App.java
+++ Aula 05/testesAula05/src/App.java
@@ -1,7 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        int resultado = 0;
+
         System.out.println("Tarefa 1");
-        int resultado = 10/0;
+        
+        try{
+            resultado = 10/0;
+        }
+        catch(Exception e){
+            System.out.println("Não foi possível calcular");
+        }
+
+
         System.out.println("Tarefa 2");
         System.out.println("Tarefa 3");
         System.out.println("Tarefa 4");
