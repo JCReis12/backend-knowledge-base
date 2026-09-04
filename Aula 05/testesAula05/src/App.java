4348871895a2fdf437fd44976c76ee6bd01426ce
João Carlos <joaocarlosreis2007@gmail.com>
2026-09-04 20:21:51 -0300

    feat: adição de teste de erro proposital para visualizar exception

--- Aula 05/testesAula05/src/App.java
+++ Aula 05/testesAula05/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Tarefa 1");
+        int resultado = 10/0;
+        System.out.println("Tarefa 2");
+        System.out.println("Tarefa 3");
+        System.out.println("Tarefa 4");
+        System.out.println(resultado);
     }
 }
