57daab2fed1b140f6728194519894dd04bbc9a89
João Carlos <joaocarlosreis2007@gmail.com>
2026-09-18 19:21:53 -0300

    feat: adição de dados pré-fixados referente ao zero absoluto

--- Aula 05/CloneExercicios/MiniProjetosJAVA/ConversorTemperatura/src/utils/Converter.java
+++ Aula 05/CloneExercicios/MiniProjetosJAVA/ConversorTemperatura/src/utils/Converter.java
@@ -1,3 +1,10 @@
+package utils;
+
 public class Converter {
+
+    public static final double ZERO_ABSOLUTO_KELVIN = 0;
+    public static final double ZERO_ABSOLUTO_CELSIUS = -273;
+    public static final double ZERO_ABSOLUTO_FAHRENHEIT = -459;
+
 
 }
