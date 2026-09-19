cad9a8d3997bda328a5d24f13412f49f8e851925
João Carlos <joaocarlosreis2007@gmail.com>
2026-09-18 22:59:18 -0300

    feat: adição da lógica inicial com atributos, metodos e construtor

--- Aula 05/CloneExercicios/MiniProjetosJAVA/ContaBancariaBasico/src/ContaBancaria.java
+++ Aula 05/CloneExercicios/MiniProjetosJAVA/ContaBancariaBasico/src/ContaBancaria.java
@@ -1,4 +1,33 @@
 
 public class ContaBancaria {
     
+    // Atributos
+    int numero;
+    Double saldo;
+    String nome;
+
+    public ContaBancaria(int numero, String nome){
+        this.numero = numero;
+        this.nome = nome;
+        saldo = 0.0;
+    }
+
+    // Métodos
+    public void depositar(double valor){
+
+            if(valor < 0){
+                throw new IllegalArgumentException("Valor não autorizado");
+            }
+
+            saldo += valor;
+    }
+
+    public void sacar(double valor){
+        
+            if(valor < 0 || valor > saldo){
+                throw new IllegalArgumentException("Valor não autorizado");
+            }
+
+            saldo -= valor;
+    }
 }
